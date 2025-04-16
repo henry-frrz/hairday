1255f8122568ad7246da3d74e6f4ba3170d190ce
Henrique Ferraz <henrique07ferraz@gmail.com>
2025-04-16 08:06:40 -0300

    feat: separate hours by period

--- src/scripts/modules/form/hours-load.js
+++ src/scripts/modules/form/hours-load.js
@@ -5,6 +5,8 @@ import openingHours from '../../../utils/opening-hours'
 const hours = document.querySelector('#hours')
 
 const hoursLoad = date => {
+  hours.textContent = ''
+
   const opening = openingHours.map(hour => {
     const [scheduleHour] = hour.split(':')
 
@@ -16,6 +18,8 @@ const hoursLoad = date => {
     }
   })
 
+  const hasPeriods = []
+
   opening.forEach(({ hour, available }) => {
     const hourItem = document.createElement('li')
 
@@ -23,8 +27,36 @@ const hoursLoad = date => {
     hourItem.classList.add(available ? 'hour-available' : 'hour-unavailable')
 
     hourItem.textContent = hour
+
+    const [periodHour] = hour.split(':')
+
+    if (!hasPeriods.includes('Manhã') && periodHour >= 6 && periodHour <= 12) {
+      addPeriodHeader('Manhã')
+      hasPeriods.push('Manhã')
+    }
+
+    if (!hasPeriods.includes('Tarde') && periodHour >= 12 && periodHour <= 18) {
+      addPeriodHeader('Tarde')
+      hasPeriods.push('Tarde')
+    }
+
+    if (!hasPeriods.includes('Noite') && periodHour >= 18) {
+      addPeriodHeader('Noite')
+      hasPeriods.push('Noite')
+    }
+
     hours.append(hourItem)
   })
+}
+
+const addPeriodHeader = period => {
+  const periodHeader = document.createElement('span')
+
+  periodHeader.classList.add('hour-period')
+
+  periodHeader.textContent = period
+
+  hours.append(periodHeader)
 }
 
 export default hoursLoad
