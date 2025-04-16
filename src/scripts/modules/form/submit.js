6c159732950ee0f6333634a2a4dc0324a7bc6319
Henrique Ferraz <henrique07ferraz@gmail.com>
2025-04-16 08:08:56 -0300

    feat: submit selected time

--- src/scripts/modules/form/submit.js
+++ src/scripts/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 
 const form = document.querySelector('form')
+const clientName = document.querySelector('#client')
 const date = document.querySelector('#date')
 
 const inputToday = dayjs().format('YYYY-MM-DD')
@@ -12,5 +13,29 @@ date.max = dayjs().add(1, 'month').format('YYYY-MM-DD')
 form.onsubmit = event => {
   event.preventDefault()
 
-  console.log('Enviado')
+  try {
+    const name = clientName.value.trim()
+
+    if (!name) {
+      return alert('Informe o nome do cliente!')
+    }
+
+    const hourSelected = document.querySelector('.hour-selected')
+
+    if (!hourSelected) {
+      return alert('Selecione um horário')
+    }
+
+    const [hour] = hourSelected.innerText.split(':')
+
+    const when = dayjs(selectedDate.value).add(hour, 'hour')
+
+    return {
+      id: new Date().getTime(),
+      name,
+      when,
+    }
+  } catch (error) {
+    alert('Mão foi possível realizar o agendamento.')
+  }
 }
