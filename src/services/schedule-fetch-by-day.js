938f5d7bda1c47eb53a248c1926ae7bc1b6ae4c3
Henrique Ferraz <henrique07ferraz@gmail.com>
2025-04-16 08:10:18 -0300

    feat: fetch appointments from API

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs'
+import apiConfig from './api-config'
+
+const scheduleFetchByDay = async ({ date }) => {
+  try {
+    const response = await fetch(`${apiConfig.baseURL}/schedules`)
+
+    const data = await response.json()
+
+    const dailySchedules = data.filter(schedule => {
+      return dayjs(date).isSame(schedule.when, 'day')
+    })
+
+    return dailySchedules
+  } catch (error) {}
+}
+
+export default scheduleFetchByDay
